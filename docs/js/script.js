281902a90e7f94f333f0a398e9f84f4d6c5da662
OksanaZhuravel <opuseva@mail.ru>
2021-09-30 17:29:03 +0300

    changed js

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,60 +1,16 @@
-// $(document).ready(function () {
-//   $('.nav__lin').on('click', function (e) {
-//     var anchor = $(this);
-//     $('html, body')
-//       .stop()
-//       .animate(
-//         {
-//           scrollTop: $(anchor.attr('href')).offset().top,
-//         },
-//         777
-//       );
-//     e.preventDefault();
-//     return false;
-//   });
-// });
+function testWebP(callback) {
+  var webP = new Image();
+  webP.onload = webP.onerror = function () {
+    callback(webP.height == 2);
+  };
+  webP.src =
+    'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
+}
 
-// <div class="button-block">
-//   <button class="button">Прокрутить вниз 👇</button>
-// </div>
-// <section class="section"></section>
-// <footer class="footer" id="footer"></footer>
-
-// body {
-//   margin: 0;
-//   font-family: sans-serif;
-// }
-// .button-block {
-//   display: flex;
-//   align-items: center;
-//   justify-content: center;
-//   padding: 10px;
-// }
-// .button {
-//   padding: 10px 20px;
-//   background-color: tomato;
-//   border: none;
-//   color: #fff;
-// }
-// .section {
-//   height: 100vh;
-//   background-color: pink;
-// }
-// .footer {
-//   height: 100vh;
-//   background-color: #333;
-// }
-// function scrollTo(element) {
-//   window.scroll({
-//     left: 0,
-//     top: element.offsetTop,
-//     behavior: 'smooth'
-//   })
-// }
-
-// var button = document.querySelector('.button');
-// var footer = document.querySelector('.footer');
-
-// button.addEventListener('click', () => {
-//   scrollTo(footer);
-// })
+testWebP(function (support) {
+  if (support == true) {
+    document.querySelector('body').classList.add('webp');
+  } else {
+    document.querySelector('body').classList.add('no-webp');
+  }
+});
